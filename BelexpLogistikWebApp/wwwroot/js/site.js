9222e540d9feb08c9a325ed72e34da32e5434989
Daria Chepikova <8grkc8@gmail.com>
2020-04-26 15:07:30 +0300

    NewVers

--- BelexpLogistikWebApp/wwwroot/js/site.js
+++ BelexpLogistikWebApp/wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function ButtClickOrdersNotEnd() {
+    document.getElementsByClassName('informationOrdersEnd')[0].style = "display: none;";
+    document.getElementsByClassName('informationOrdersNotEnd')[0].style = "display: table; width: 100%";
+    document.getElementsByClassName('inputOrderNotEnd')[0].style = "background: white";
+    document.getElementsByClassName('inputOrderEnd')[0].style = "background:  #f0f0f5";
+}
+function ButtClickOrdersEnd() {
+    document.getElementsByClassName('informationOrdersNotEnd')[0].style = "display: none;";
+    document.getElementsByClassName('informationOrdersEnd')[0].style = "display: table; width: 100%";
+    document.getElementsByClassName('inputOrderEnd')[0].style = "background: white";
+    document.getElementsByClassName('inputOrderNotEnd')[0].style = "background:  #f0f0f5";
+}
+function ViewAllDrivers()
+{
+    document.getElementsByClassName('visibleHidDrivers')[0].style = "display: table; width: 100%";
+    document.getElementsByClassName('aPositionLeft')[0].style = "display: none";
+    document.getElementsByClassName('aPositionRight')[0].style = "display: block";
+}
+function ViewNotAllDrivers() {
+    document.getElementsByClassName('visibleHidDrivers')[0].style = "display: none";
+    document.getElementsByClassName('aPositionRight')[0].style = "display: none";
+    document.getElementsByClassName('aPositionLeft')[0].style = "display: block";
+
+}
+
+function ViewAllDrivers2() {
+    document.getElementsByClassName('visibleHidDrivers2')[0].style = "display: table; width: 100%";
+    document.getElementsByClassName('aPositionLeft2')[0].style = "display: none";
+    document.getElementsByClassName('aPositionRight2')[0].style = "display: block";
+}
+function ViewNotAllDrivers2() {
+    document.getElementsByClassName('visibleHidDrivers2')[0].style = "display: none";
+    document.getElementsByClassName('aPositionRight2')[0].style = "display: none";
+    document.getElementsByClassName('aPositionLeft2')[0].style = "display: block";
+
+}
