6124acd85894530ce381405d1f681b99f6c9bf96
KQQ <kqqwm1@gmail.com>
2020-06-10 17:36:24 +0300

    Logick for Views

--- BelexpLogistikWebApp/Controllers/OwnerController.cs
+++ BelexpLogistikWebApp/Controllers/OwnerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BelexpLogistikWebApp.Controllers
@@ -17,6 +18,55 @@ namespace BelexpLogistikWebApp.Controllers
         {
             var owners = db.CarOwner;
             return View(owners);
+        }
+        [HttpGet]
+        [Authorize(Roles = "Логист")]
+        public ActionResult Delete(int id)
+        {
+            CarOwner carOwner = db.CarOwner.Find(id);
+            if (carOwner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carOwner);
+        }
+
+        private ActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Логист")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CarOwner carOwner = db.CarOwner.Find(id);
+            if (carOwner == null)
+            {
+                return HttpNotFound();
+            }
+            db.CarOwner.Remove(carOwner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit()
+        {
+            var owners = db.CarOwner;
+            return View(owners);
+        }
+        [HttpGet]
+        [Authorize(Roles = "Логист")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Create(CarOwner carOwner)
+        {
+            db.CarOwner.Add(carOwner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
